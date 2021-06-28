0912a55fe94ea02ee7a3d03c8de0e2ea8eab5c46
Steven Tjoa <steventjoa89@gmail.com>
2021-06-28 23:43:57 +0700

    Add new product sodium thio.

--- assets/js/product-list.js
+++ assets/js/product-list.js
@@ -1,6 +1,7 @@
 // USD RATE DAILY
 let usdIdrRate = 14250;
 let myRate = 0.15;
+let myRangePrice = 7.5;
 
 // IMAGE LOCATION
 let imageUrl = `assets/img/products/`;
@@ -11,34 +12,70 @@ let productList =
 	<product>
 		<id>1</id>
 		<name>Trisodium Phosphate</name>
-		<category>General</category>
+		<category>Phosphate</category>
+		<formula>Na3PO4</formula>
+		<application>Industrial</application>
+		<purity>98%</purity>
 		<brand>-</brand>
-		<grade>Industrial Grade</grade>
 		<origin>China</origin>
-		<package>Sak</package>
+		<package>Bag</package>
 		<weight>25</weight>
-		<price>0.395</price>
-		<description>-</description>
+		<color>Putih</color>
+		<appearance>Bubuk Putih</appearance>
+		<price>0.42</price>
+		
 		<images>
 			<image>${imageUrl}trisodium-phosphate-1.jpg</image>
 			<image>${imageUrl}trisodium-phosphate-2.jpg</image>
 		</images>
+
+		<description>
+			&lt;ol&gt;
+				&lt;li&gt;Penghambat skala boiler.&lt;/li&gt;
+				&lt;li&gt;Memperbaiki untuk percetakan dan pencelupan bahan fabric pakaian.&lt;/li&gt;
+				&lt;li&gt;Mercerisasi bahan fabric pakaian.&lt;/li&gt;
+				&lt;li&gt;Bahan pengental lateks karet.&lt;/li&gt;
+				&lt;li&gt;Bahan pelembut untuk pencelupan kertas.&lt;/li&gt;
+				&lt;li&gt;Penghilang minyak dan dekontminasi kimia industri metallic.&lt;/li&gt;
+				&lt;li&gt;Akselerator yang sangat baik dalam solusi fotografi.&lt;/li&gt;
+				&lt;li&gt;Perekat kertas lilin berbasis-pH.&lt;/li&gt;
+			&lt;/ol&gt;
+		</description>
 	</product>
+
 	<product>
-		<id>2</id>
-		<name>Sodium Bicarbonate</name>
-		<category>food</category>
-		<brand>Malan</brand>
-		<grade>Industrial Grade</grade>
-		<origin>China</origin>
-		<package>Sak</package>
-		<weight>25</weight>
-		<price>150.000,00</price>
-		<description>Sodium Bicarbonate description.</description>
-		<images>
-			<image>${imageUrl}sodium-bicarbonate-malan-1.jpg</image>
-			<image>${imageUrl}sodium-bicarbonate-malan-2.jpg</image>
-		</images>
+    <id>2</id>
+    <name>Sodium Thiosulphate Anhydrous</name>
+    <category>Sulphate</category>
+    <formula>Na2S2O3.5H2O</formula>
+    <application>plating, leather, sulfur dye</application>
+    <purity>99%</purity>
+    <brand>-</brand>
+    <origin>China</origin>
+    <package>Bag</package>
+    <weight>25</weight>
+    <color>Putih</color>
+    <appearance>Kristal Transparan</appearance>
+    <price>0.42</price>
+
+    <images>
+			<image>${imageUrl}sodium-thiosulphate-1.jpg</image>
+			<image>${imageUrl}sodium-thiosulphate-2.jpg</image>
+    </images>
+
+    <description>
+			&lt;ol&gt;
+				&lt;li&gt;Digunakan dalam proses fotografi&lt;/li&gt;
+				&lt;li&gt;Aplikasi lempeng listrik dalam baja/besi&lt;/li&gt;
+				&lt;li&gt;Digunakan untuk kulit sistentis&lt;/li&gt;
+				&lt;li&gt;Digunakan untuk pengujian PH zat pemutih&lt;/li&gt;
+				&lt;li&gt;Dalam pengolahan air limbah, menetralkan air yang diputihkan dan diklorinasi&lt;/li&gt;
+				&lt;li&gt;Menurunkan kadar klorin di kolam renang dan spa&lt;/li&gt;
+				&lt;li&gt;Digunakan dalam pemutih&lt;/li&gt;
+				&lt;li&gt;Digunakan dalam industri kertas&lt;/li&gt;
+				&lt;li&gt;Digunakan untuk menghilangkan bekas iodine&lt;/li&gt;
+			&lt;/ol&gt;
+    </description>
 	</product>
 </product-list>
 `

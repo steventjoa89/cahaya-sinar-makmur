90db7c4a4e0590466f77e42bcc53c7377d1a1baf
Steven Tjoa <steventjoa89@gmail.com>
2021-07-03 15:11:36 +0700

    Product category now can be choosen by url.

--- assets/js/product.js
+++ assets/js/product.js
@@ -12,24 +12,32 @@ let getAllProductList = () => {
 	let totalProductsCount = products.length;
 	document.getElementById("total-product-categories-count").innerHTML = totalProductsCount;
 
-	// Get All Products Categories
+	// // Get All Products Categories
 	var productCategories = new Array();
+	console.log(products);
 	products.forEach(function (product) {
 		product.categories.category.forEach(function(category){
-			productCategories.push(category);
+			productCategories.push(category.toLowerCase());
 		})		
 	});
 
-	// Get Unique Product Category Array
+	// // Get Unique Product Category Array
 	var productCategoryUnique = productCategories.filter(onlyUnique);
 	productCategoryUnique.sort();
 
-	// Printing Product Categories
-	for(const value of productCategoryUnique){
-		const div = document.createElement('div');
-		div.innerHTML = `<li data-filter=".filter-${value.toLowerCase()}" style="margin: 0px; padding: 0 3px 5px 0; font-weight: 200;">${value} </li><small style="color: #aaaaaa;">(${countOccurrences(productCategories, value)})</small><br>`;
-		document.getElementById("product-filter").appendChild(div);
-	}
+	// // Printing Product Categories
+	// for(const value of productCategoryUnique){
+	// 	const div = document.createElement('div');
+	// 	div.innerHTML = `<li data-filter=".filter-${value.toLowerCase()}" style="margin: 0px; padding: 0 3px 5px 0; font-weight: 200;">${value} </li><small style="color: #aaaaaa;">(${countOccurrences(productCategories, value)})</small><br>`;
+	// 	document.getElementById("product-filter").appendChild(div);
+	// }
+
+	// Count Food Category
+	document.getElementById("total-product-category-food").innerHTML = countOccurrences(productCategories, "food");
+	document.getElementById("total-product-category-industrial").innerHTML = countOccurrences(productCategories, "industrial");
+	document.getElementById("total-product-category-solvent").innerHTML = countOccurrences(productCategories, "solvent");
+	document.getElementById("total-product-category-uncategorized").innerHTML = countOccurrences(productCategories, "uncategorized");
+
 
 	// Printing All Products
 	for(const product of products){
@@ -53,19 +61,27 @@ let getAllProductList = () => {
 		`;
 		document.getElementById('product-list').appendChild(div);
 	}
+
+	window.addEventListener('load', function () {
+		checkForCategorySearch(productCategoryUnique);
+	});
+}
+
+let checkForCategorySearch = (productCategoryUnique) => {
+	
+
+	console.log();
+	if(new URLSearchParams(window.location.search).get("category")!=null){
+		// Decode First
+		let searchCategory = decodeURIComponent(new URLSearchParams(window.location.search).get("category"));
+
+		for(const value of productCategoryUnique){
+			if(value.toLowerCase() == searchCategory.toLowerCase()){
+				document.getElementById(`category-${value}`).click();
+				break;
+			}
+		}
+	}
 }
 
 getAllProductList();
-
-// TODO - - - 
-// Let Product Search
-// let forms = document.querySelectorAll('.product-search');
-// forms.forEach( function(e) {
-//     e.addEventListener('submit', function(event) {
-//       event.preventDefault();
-
-// 	  console.log("Hello World");
-// 	  // Set Filter to All
-// 	  // Set Custom Filter
-//     });
-//   });

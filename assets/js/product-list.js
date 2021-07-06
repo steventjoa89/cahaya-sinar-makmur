94be95201e997553851c48a0747624fb7e5ccbc2
Steven Tjoa <steventjoa89@gmail.com>
2021-07-06 22:42:34 +0700

    Done publish solvent products and adding google search console header.

--- assets/js/product-list.js
+++ assets/js/product-list.js
@@ -2,7 +2,27 @@ let jsonProductList = `
 {
   "products": {
     "product": [
-		{"id": 1,"name": "Trisodium Phosphate/TSP","categories": {"category": ["Industrial"]},"purity": "98% Min","formula": "Na3PO4","country": "China","brand": "-","package": "Bag","content": "25","grades": {"grade": ["Technical"]},"appearance": "Powder","color": "White","description": "Trisodium phosphate (TSP) is the inorganic compound with the chemical formula Na3PO4. It is a white, granular or crystalline solid, highly soluble in water, producing an alkaline solution. TSP is used as a cleaning agent, builder, lubricant, food additive, stain remover, and degreaser.","images": { "image": [ "trisodium-phosphate-1.jpg", "trisodium-phosphate-2.jpg"]}}
+		{
+      "id": 1,"name": "Trisodium Phosphate/TSP","categories": {"category": ["Industrial"]},"purity": "98% Min","formula": "Na3PO4","country": "China","brand": "-","package": "Bag","content": "25","grades": {"grade": ["Technical"]},"appearance": "Powder","color": "White","description": "Trisodium phosphate (TSP) is the inorganic compound with the chemical formula Na3PO4. It is a white, granular or crystalline solid, highly soluble in water, producing an alkaline solution. TSP is used as a cleaning agent, builder, lubricant, food additive, stain remover, and degreaser.","images": { "image": [ "trisodium-phosphate-1.jpg", "trisodium-phosphate-2.jpg"]}
+    },
+    {
+      "id": 2,"name": "Etanol / Ethyl Alcohol","categories": {"category": ["Solvent"]},"purity": "70-99% Min","formula": "C2H5OH","country": "China","brand": "-","package": "Drum","content": "200","grades": {"grade": ["Food"]},"appearance": "Liquid","color": "Colorless","description": "Ethanol has a wide range of uses. Ethanol can be used to make acetic acid, beverages, flavors, dyes, fuels, etc. Ethanol with a volume fraction of 70% to 75% is also commonly used in medical treatment as a disinfectant, etc. It is widely used in national defense chemical industry, medical and health, food industry, industrial and agricultural production.","images": { "image": [ "ethyl-alcohol-1.jpg", "ethyl-alcohol-2.jpg"]}
+    },
+    {
+      "id": 3,"name": "Methanol / Methyl Alcohol","categories": {"category": ["Solvent"]},"purity": "70-99% Min","formula": "CH3OH","country": "China","brand": "-","package": "Drum","content": "160","grades": {"grade": ["Technical"]},"appearance": "Liquid","color": "Colorless","description": "Methanol is widely used, it is the basic organic raw material and high-quality fuel. Mainly used in chemicals, plastics, and other fields. It can be used for the synthesis of many organic products, such as formaldehyde, acetic acid, methyl chloride, methyl ammonia, sulfuric acid, dimethyl ester and so on. It is also one of the important raw materials for pesticides and medicines. It can be used as a new type of clean fuel after deep processing, usually also added to gasoline in combination.","images": { "image": [ "methyl-alcohol-1.jpg", "methyl-alcohol-2.jpg"]}
+    },
+    {
+      "id": 4,"name": "ISO Propyl Alcohol (IPA)","categories": {"category": ["Solvent"]},"purity": "70-99%","formula": "C3H8O","country": "China","brand": "-","package": "Drum","content": "160","grades": {"grade": ["Food & Industrial"]},"appearance": "Liquid","color": "Colorless","description": "Isopropanol is an important chemical products and raw materials. It is mainly applied to various fields including pharmaceutical,cosmetics, plastics, fragrances, paint as well as being used as the dehydrating agent and cleaning agent in and electronics industry. It can also be used as the reagent for determination of barium, calcium, magnesium, nickel, potassium,sodium and strontium. It can also be used as the reference material of chromatographic analysis.","images": { "image": [ "iso-propyl-alcohol-1.jpg", "iso-propyl-alcohol-2.jpg"]}
+    },
+    {
+      "id": 5,"name": "Perchloroethylene / PCE","categories": {"category": ["Solvent"]},"purity": "99.9% Min","formula": "C2Cl4","country": "China","brand": "-","package": "Drum","content": "300","grades": {"grade": ["Technical"]},"appearance": "Liquid","color": "Colorless","description": "Dry clean ,Organic Impregnant,Cleanout,Desulfurize. It is mainly used as organic solvent, dry cleanser and metal degreaser as well as anthelmintic. It can be applied to all kinds of nature and synthetic fabrics as a dry cleanser, and the fabric will be soft, glossy, odorless, fadeproof, in shape and free of damage after being washed.It can also be used as fatty extractant and the raw material for HCFC-123.HCFC-124,HCFC-125,HCFC-134a etc.","images": { "image": [ "perchloroethylene-1.jpg", "perchloroethylene-2.jpg"]}
+    },
+    {
+      "id": 6,"name": "Tetrachloroethylene","categories": {"category": ["Solvent"]},"purity": "0.999","formula": "C2Cl4","country": "China","brand": "-","package": "Drum","content": "300","grades": {"grade": ["Technical"]},"appearance": "Liquid","color": "Colorless","description": "Tetrachloroethylene is an excellent solvent for organic materials. Otherwise it is volatile, highly stable, and nonflammable. For these reasons, it is widely used in dry cleaning. Usually as a mixture with other chlorocarbons, it is also used to degrease metal parts in the automotive and other metalworking industries. It appears in a few consumer products including paint strippers and spot removers.  It is used in neutrino detectors where a neutrino interacts with a neutron in the chlorine atom and converts it to a proton to form argon.","images": { "image": [ "tetrachloroethylene-1.jpg", "tetrachloroethylene-2.jpg"]}
+    }
+
+
+
     ]
   }
 }

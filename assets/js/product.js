ec5ddcd03b2264a7f51deafa1f8a3827604818e4
Steven Tjoa <steventjoa89@gmail.com>
2021-07-08 01:22:05 +0700

    Adding product to load specific product on page.

--- assets/js/product.js
+++ assets/js/product.js
@@ -4,40 +4,63 @@ function onlyUnique(value, index, self) {
 
 let countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 
+var allProductCategories = new Array();
+let products = JSON.parse(jsonProductList).products.product;
+
 let getAllProductList = () => {
-	let products = JSON.parse(jsonProductList).products.product;
-	
-
-	// Get Total Products
-	let totalProductsCount = products.length;
-	document.getElementById("total-product-categories-count").innerHTML = totalProductsCount;
-
-	// // Get All Products Categories
-	var productCategories = new Array();
 	console.log(products);
 	products.forEach(function (product) {
 		product.categories.category.forEach(function(category){
-			productCategories.push(category.toLowerCase());
+			allProductCategories.push(category.toLowerCase());
 		})		
 	});
 
-	// // Get Unique Product Category Array
-	var productCategoryUnique = productCategories.filter(onlyUnique);
-	productCategoryUnique.sort();
+	// Check first whether all or per category
+	if(new URLSearchParams(window.location.search).get("category")!=null){			// if only display one category
+		// Decode First
+		let searchCategory = decodeURIComponent(new URLSearchParams(window.location.search).get("category"));
+		displayProduct(searchCategory);
+	}else{																																			// if display all
+		displayProductCategoryHtml(productCategory);
+		displayProductHtml(products);
+	}
+}
 
-	// // Printing Product Categories
-	// for(const value of productCategoryUnique){
-	// 	const div = document.createElement('div');
-	// 	div.innerHTML = `<li data-filter=".filter-${value.toLowerCase()}" style="margin: 0px; padding: 0 3px 5px 0; font-weight: 200;">${value} </li><small style="color: #aaaaaa;">(${countOccurrences(productCategories, value)})</small><br>`;
-	// 	document.getElementById("product-filter").appendChild(div);
-	// }
+var productCategory = ['Food', 'Industrial', 'Solvent'];
 
-	// Count Food Category
-	document.getElementById("total-product-category-food").innerHTML = countOccurrences(productCategories, "food");
-	document.getElementById("total-product-category-industrial").innerHTML = countOccurrences(productCategories, "industrial");
-	document.getElementById("total-product-category-solvent").innerHTML = countOccurrences(productCategories, "solvent");
-	// document.getElementById("total-product-category-uncategorized").innerHTML = countOccurrences(productCategories, "uncategorized");
+let displayProduct = (category) => {
+	console.log("category "+category)
+	indexOf = (arr, q) => arr.findIndex(item => q.toLowerCase() === item.toLowerCase());
+	
+	if(indexOf(productCategory, category)!=-1){
+		let singleCategory = new Array()
+		singleCategory.push(productCategory[indexOf(productCategory, category)]);
+		displayProductCategoryHtml(singleCategory, false);
 
+		const product = products.filter(product => product.categories.category.toString().toLowerCase().indexOf(category)>-1);
+		console.log(product);
+
+		displayProductHtml(product);
+	}else{
+		displayProductCategoryHtml(productCategory);
+		displayProductHtml(products);
+	}
+}
+
+let displayProductCategoryHtml = (productCategory, isShowAll = true) => {
+	let productCategoryHtml = '';
+	if(isShowAll) productCategoryHtml += `<li data-filter="*" class="filter-active" style="margin: 0px; padding: 0 3px 5px 0; font-weight: 200;">All </li><small style="color: #aaaaaa;">(${products.length})<br>`;
+
+	let displayFilterActive = !isShowAll ? `class="filter-active"` : ``;
+	
+	for(const category of productCategory){
+		productCategoryHtml += `<li id="category-food" data-filter=".filter-${category.toLowerCase()}" ${displayFilterActive} style="margin: 0px; padding: 0 3px 5px 0; font-weight: 200;">${category} </li><small style="color: #aaaaaa;">(${countOccurrences(allProductCategories, category.toLowerCase())})</small><br>`;
+	}
+	document.getElementById("product-filter").innerHTML = productCategoryHtml;
+}
+
+let displayProductHtml = (products) => {
+	console.log("printing all products");
 
 	// Printing All Products
 	for(const product of products){
@@ -47,6 +70,7 @@ let getAllProductList = () => {
 			prodCategories.push("filter-"+category.toLowerCase());
 		}
 		div.className = "col-lg-4 col-md-6 portfolio-wrap "+prodCategories.join(" ");
+
 		div.innerHTML = `
 		<span><center>${product.name}</center></span>
 		<div class="portfolio-item">
@@ -59,29 +83,28 @@ let getAllProductList = () => {
 			</div>
 		</div>
 		`;
+
 		document.getElementById('product-list').appendChild(div);
 	}
 
-	window.addEventListener('load', function () {
-		checkForCategorySearch(productCategoryUnique);
-	});
-}
 
-let checkForCategorySearch = (productCategoryUnique) => {
+
+
 	
-
-	console.log();
-	if(new URLSearchParams(window.location.search).get("category")!=null){
-		// Decode First
-		let searchCategory = decodeURIComponent(new URLSearchParams(window.location.search).get("category"));
-
-		for(const value of productCategoryUnique){
-			if(value.toLowerCase() == searchCategory.toLowerCase()){
-				document.getElementById(`category-${value}`).click();
-				break;
-			}
-		}
-	}
+	// div.className = "col-lg-4 col-md-6 portfolio-wrap "+'12345';
+	// div.innerHTML = `
+	// 	<span><center>12134</center></span>
+	// 	<div class="portfolio-item">
+	// 		<img src="assets/img/products/123" class="img-fluid" alt="123">
+	// 		<div class="portfolio-info">
+	// 			<h3>1234</h3>
+	// 			<div>
+	// 				<a href="product-details.html?id=123" title="Portfolio Details"><i class="bx bx-search"></i></a>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// 	`;
+	// 	document.getElementById('product-list').appendChild(div);
 }
 
 getAllProductList();

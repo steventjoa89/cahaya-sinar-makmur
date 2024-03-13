4c0c82e5750d46a242f7edb18718dde871b314e9
Steven Tjoa <steven.tjoa@allianz.co.id>
2024-03-13 19:39:48 +0700

    removing tailwindcss.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1 className="hello">Hello World</h1>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;

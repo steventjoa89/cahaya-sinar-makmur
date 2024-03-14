b7257a2cd43eb67852ea586c4ac25d5c65a7be90
Steven Tjoa <steven.tjoa@allianz.co.id>
2024-03-14 17:05:41 +0700

    Client done.

--- src/data/info.js
+++ src/data/info.js
@@ -5,6 +5,15 @@ export const COMPANY_ADDRESS_3 = "Indonesia";
 export const COMPANY_PHONE = "+62 819 058 7 5858";
 export const COMPANY_EMAIL = "steventjoa89@gmail.com";
 
+export const CLIENTS = [
+  { name: "myob", image: "/images/clients/client-1.png" },
+  { name: "myob", image: "/images/clients/client-2.png" },
+  { name: "myob", image: "/images/clients/client-3.png" },
+  { name: "myob", image: "/images/clients/client-4.png" },
+  { name: "myob", image: "/images/clients/client-5.png" },
+  { name: "myob", image: "/images/clients/client-6.png" },
+];
+
 export const USEFUL_LINKS = [
   { name: "Home", link: "/" },
   { name: "About Us", link: "/about-us" },
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero";
+import Clients from "../../components/Clients";
 
 function HomePage() {
   return (
     <>
       <Header />
       <Hero />
+      <div id="main">
+        <Clients />
+        
+      </div>
+
       1<br />
       1<br />
       1<br />
